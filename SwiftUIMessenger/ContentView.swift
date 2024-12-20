a48d49aa05b3648a4d02855344796b8a0f3cebcc
Siran Li <siranli411@gmail.com>
2024-12-20 18:09:42 -0500

    Initial Commit

---
+++ SwiftUIMessenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIMessenger
+//
+//  Created by Siran Li on 12/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMessenger/SwiftUIMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMessengerApp.swift
+//  SwiftUIMessenger
+//
+//  Created by Siran Li on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
