e3b0426eaaaa6423ebb614c058b167a40ad978f2
Siran Li <siranli411@gmail.com>
2024-12-23 19:34:29 -0500

    add UpdatesTabScreen. update color asset

---
+++ SwiftUIMessenger/UpdatesTabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  UpdatesTabScreen.swift
+//  SwiftUIMessenger
+//
+//  Created by Siran Li on 12/20/24.
+//
+
+import SwiftUI
+
+struct UpdatesTabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdatesTabScreen()
+}
